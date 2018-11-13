47fe5bc4039afca8594483e6ada4eefcd4387c08
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-13 15:47:32 -0800

    Update requests controller

--- cats99/app/controllers/cat_rental_requests_controller.rb
+++ cats99/app/controllers/cat_rental_requests_controller.rb
@@ -9,4 +9,18 @@ class CatRentalRequestsController < ApplicationController
     @request = CatRentalRequest.find(params[:id])
     render :show
   end
+  
+  def new
+    @request = CatRentalRequest.new
+    render :new
+  end
+  
+  def create
+    @request = CatRentalRequest.new(rental_params)
+    if @request.save
+      redirect_to :cat_rental_request_url(params[:id])
+    else
+      render json: @request.errors.full_messages, status: 422
+    end  
+  end
 end
